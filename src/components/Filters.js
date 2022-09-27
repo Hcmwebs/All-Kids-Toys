82050e887745a0639103e8821083f44404d81438
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-09-28 06:14:48 +1300

    set up all the filters and the corresponding functionality

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -23,7 +23,6 @@ const Filters = () => {
 	const categories = getUniqueValues(all_products, 'category')
 	const companies = getUniqueValues(all_products, 'company')
 	const colors = getUniqueValues(all_products, 'colors')
-	console.log(categories)
 
 	return (
 		<Wrapper>
@@ -63,7 +62,100 @@ const Filters = () => {
 					</div>
 
 					{/* end of categories */}
+
+					{/* companies */}
+
+					<div className='form-control'>
+						<h5>company</h5>
+						<select
+							name='company'
+							value={company}
+							onChange={updateFilters}
+							className='company'>
+							{companies.map((c, index) => {
+								return (
+									<option key={index} value={c}>
+										{c}
+									</option>
+								)
+							})}
+						</select>
+					</div>
+
+					{/* end of companies */}
+
+					{/* colors */}
+					<div className='form-control'>
+						<h5>colors</h5>
+						<div className='colors'>
+							{colors.map((c, index) => {
+								if (c === 'all') {
+									return (
+										<button
+											key={index}
+											name='color'
+											onClick={updateFilters}
+											data-color='all'
+											className={`${
+												color === 'all' ? 'all-btn active' : 'all-btn'
+											}`}>
+											{' '}
+											all
+										</button>
+									)
+								}
+								return (
+									<button
+										key={index}
+										name='color'
+										style={{ background: c }}
+										className={`${
+											color === c ? 'color-btn active' : 'color-btn'
+										}`}
+										data-color={c}
+										onClick={updateFilters}>
+										{color === c ? <FaCheck /> : null}
+									</button>
+								)
+							})}
+						</div>
+					</div>
+
+					{/* end of colors */}
+
+					{/* price */}
+					<div className='form-control'>
+						<h5>price</h5>
+						<p className='price'>{formatPrice(price)}</p>
+						<input
+							type='range'
+							name='price'
+							onChange={updateFilters}
+							min={min_price}
+							max={max_price}
+							value={price}
+						/>
+					</div>
+					{/* end of price */}
+
+					{/* shipping */}
+					<div className='form-control shipping'>
+						<label htmlFor='shipping'>Free shipping</label>
+						<input
+							type='checkbox'
+							name='shipping'
+							id='shipping'
+							onChange={updateFilters}
+							checked={shipping}
+						/>
+					</div>
+
+					{/* end of shipping */}
 				</form>
+
+				<button type='button' className='clear-btn' onClick={clearFilters}>
+					Clear filters
+				</button>
 			</div>
 		</Wrapper>
 	)
