ac9fcecb794837c13230408cb459b8aaefbacb9b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-12-25 06:32:52 +1300

    added local

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,17 @@ import {
 	COUNT_CART_TOTALS,
 } from '../actions'
 
+const getLocalStorage = () => {
+	let cart = localStorage.getItem('cart')
+	if (cart) {
+		return JSON.parse(localStorage.getItem('cart'))
+	} else {
+		return []
+	}
+}
+
 const initialState = {
-	cart: [],
+	cart: getLocalStorage(),
 	total_items: 0,
 	total_amount: 0,
 	shipping_fee: 534,
@@ -25,17 +34,22 @@ export const CartProvider = ({ children }) => {
 		dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
 	}
 
-// remove item
+	// remove item
 
-const removeItem = (id) => {}
+	const removeItem = (id) => {}
 
-// toggleAmount
-const toggleAmount = (id, value) => {}
+	// toggleAmount
+	const toggleAmount = (id, value) => {}
 
-// clear cart
-const clearCart = (id) => {}
+	// clear cart
+	const clearCart = (id) => {}
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify())
+	}, [state.cart])
 	return (
-		<CartContext.Provider value={{ ...state, addToCart, removeItem, toggleAmount, clearCart}}>
+		<CartContext.Provider
+			value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}>
 			{children}
 		</CartContext.Provider>
 	)
