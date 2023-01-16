2b13315ab55bc1d8eda759ab3e487521314cb02d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-17 03:59:03 +1300

    used .ENV variables
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-//dev-j7ugh2mb.us.auth0.com
-//cu9kJp99KpGr8dB8YY6NufKwSru3D6AG
+
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -18,13 +17,15 @@ ReactDOM.render(
 			clientId={`${process.env.REACT_APP_REACTSTORE_CLIENTID}`}
 			redirectUri={window.location.origin}
 			cacheLocation='localstorage'>
-			<ProductsProvider>
-				<FilterProvider>
-					<CartProvider>
-						<App />
-					</CartProvider>
-				</FilterProvider>
-			</ProductsProvider>
+			<UserProvider>
+				<ProductsProvider>
+					<FilterProvider>
+						<CartProvider>
+							<App />
+						</CartProvider>
+					</FilterProvider>
+				</ProductsProvider>
+			</UserProvider>
 		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
