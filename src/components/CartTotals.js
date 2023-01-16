25adc2821460f627ff74d1e7101f2c137aa5bc95
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-17 03:58:04 +1300

    worked on user login /logout with Auth0
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom'
 
 const CartTotals = () => {
 	const { total_amount, shipping_fee } = useCartContext()
+	const { myUser, loginWithRedirect } = useUserContext()
+
 	return (
 		<Wrapper>
 			<div>
@@ -23,9 +25,16 @@ const CartTotals = () => {
 						<span>{formatPrice(total_amount + shipping_fee)}</span>
 					</h4>
 				</article>
-				<Link to='/checkout' className='btn'>
-					Proceed to cart
-				</Link>
+
+				{myUser ? (
+					<Link to='/checkout' className='btn'>
+						Proceed to cart
+					</Link>
+				) : (
+					<button type='button' className='btn' onClick={loginWithRedirect}>
+						login
+					</button>
+				)}
 			</div>
 		</Wrapper>
 	)
